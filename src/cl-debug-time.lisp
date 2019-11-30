5ed42934db9c041d68de3229e3b8340e83f9afbc
gos-k <mag4.elan@gmail.com>
2019-12-01 02:07:59 +0900

    Refact

--- src/cl-debug-time.lisp
+++ src/cl-debug-time.lisp
@@ -10,19 +10,22 @@
   (:export :measure-time))
 (in-package :cl-debug-time)
 
+(defun %second-per-unit (unit)
+  (ecase unit
+    ((or :h :hour) (/ 1.0 3600.0))
+    ((or :m :min :minute) (/ 1.0 60.0))
+    ((or :s :sec :second) 1.0)
+    ((or :ms :msec :millisecond) 1000.0)
+    ((or :us :usec :microsecond) 1000000.0)))
+
 (defannotation measure-time (unit body)
   (:arity 2 :inline t)
   (with-gensyms (start end)
-    (let ((mul (ecase unit
-                 ((or :h :hour) (/ 1.0 3600.0))
-                 ((or :m :min :minute) (/ 1.0 60.0))
-                 ((or :s :sec :second) 1.0)
-                 ((or :ms :msec :millisecond) 1000.0)
-                 ((or :us :usec :microsecond) 1000000.0))))
-      `(let ((,start (now)))
-         (unwind-protect (progn ,body)
-           (let ((,end (now)))
-             (format *trace-output*
-                     "~,3f [~a]~%"
-                     (* ,mul (timestamp-difference ,end ,start))
-                     ,(string-downcase unit))))))))
+    `(let ((,start (now)))
+       (unwind-protect (progn ,body)
+         (let ((,end (now)))
+           (format *trace-output*
+                   "~,3f [~a]~%"
+                   (* ,(%second-per-unit unit)
+                      (timestamp-difference ,end ,start))
+                   ,(string-downcase unit)))))))
